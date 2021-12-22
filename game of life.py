2f654006c3056852eb46f71f3d8a86f8f7dcd1d3
Fitjet <andreykikin025@gmail.com>
2021-12-22 22:14:03 +0300

    Исправил ошибки в отступах

--- game of life.py
+++ game of life.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root['bg'] = '#FFFFFF'
-root.title('Фентези приключение')
+root.title('Игра в жизнь')
 root.geometry('500x500')
 root.resizable(width=False, height=False)
 
@@ -11,24 +11,24 @@ need_sleep = 0
 
 def eat():
     eat_egg = Button(text='кушать яйцо', command=eategg)
-    eat_egg.place(relx=0.50, rely=0.30, relwidth=0.25, relheight=0.1)
+    eat_egg.place(relx=0.50, rely=0.30, relwidth=0.25, relheight=0.1)
     
     eat_fish = Button(text='кушать рыбу', command=eategg)
-    eat_fish.place(relx=0.75, rely=0.30, relwidth=0.25, elheight=0.1)
+    eat_fish.place(relx=0.75, rely=0.30, relwidth=0.25, elheight=0.1)
     
     fish = 1
     egg = 5
-    info = Label(text='У вас есть: \n' + str(fish) + ' Рыбы\n' + str(egg) + ' Яичек!', bg='#FFFFFF', font=30)
+    info = Label(text='У вас есть: \n' + str(fish) + ' Рыбы\n' + 
+str(egg) + ' Яичек!', bg='#FFFFFF', font=30)
     info.place(relx=0.50, relwidth=0.5, relheight=0.3)
     if egg != 0:
         def eategg():
             egg - 1
             if need_eat == 0:
-                info = Label(text='Вы не хотите кушать!', bg='#FFFFFF', font=30)
+                info = Label(text='Вы не хотите кушать!', bg='#FFFFFF', font=30)
                 info.place(relx=0.50, relwidth=0.5, relheight=0.3)
             else:
                 need_eat - 25
-        if egg != 0:
         
 info1 = Label(text='Ваш голод: ' + str(need_eat) + '\nВы хотите спать: ' + str(need_sleep), bg='#FFFFFF', font=30)
 info1.place(relx=0.50, rely=0.70 ,relwidth=0.5, relheight=0.3)
